e1243a402c50d20767f0b47f25297f464ae41b9d
marianavigano <mariana@fdd.com.br>
2018-11-26 23:59:45 -0200

    front pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,7 @@
 				<i class="fa fa-calendar-o"></i>
 				<span>22-11-2018</span>
 			</div>
+			<img src="http://fncit.com.br/blog/wp-content/uploads/2017/10/banner-post-blog.jpg" alt="Banner" >
 			<p>
 				Lorem ipsum nisi curabitur pharetra dolor cubilia imperdiet, aliquam nulla sed faucibus ad praesent adipiscing libero, bibendum primis pellentesque dolor per hendrerit. turpis conubia mattis porta sem aenean feugiat semper tempor phasellus, vel vehicula felis scelerisque quam tortor dui curae dapibus, platea dapibus fusce porta lectus eros iaculis tellus. cubilia mollis aptent vulputate rhoncus risus aptent tortor rhoncus facilisis, class amet ad bibendum platea sagittis luctus purus, hac felis curabitur sociosqu nam nibh vestibulum orci. fringilla vestibulum aenean platea proin ornare fringilla molestie iaculis luctus, donec varius in aptent consequat euismod vulputate. 
 
